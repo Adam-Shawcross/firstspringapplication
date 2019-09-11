12046236d66b950998798ee5ff31e0578fceae3c
Adam-Shawcross <Adam.shawcross@academytrainee.com>
2019-09-11 18:49:51 +0100

    Removed redundant imports.

--- src/test/java/com/qa/controllers/NotesControllerTest.java
+++ src/test/java/com/qa/controllers/NotesControllerTest.java
@@ -8,16 +8,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
